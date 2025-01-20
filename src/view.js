2c378c2fcfa42a57978b5bdcfa04585cf3285a24
Ingsip <devsoaring@gmail.com>
2025-01-20 23:18:07 +0300

    fix

--- src/view.js
+++ src/view.js
@@ -5,41 +5,41 @@ const renderErrorss = (elements, i18n, value) => {
 
   const { feedback } = elements;
 
-	if(value === null) {
-		return;
-	}
+  if(value === null) {
+    return;
+  }
 
-	switch (value) {
-		case 'feedback.invalidUrl':
-			feedback.textContent = t(value);
-			break;
-		
-			case 'feedback.conflict':
-				feedback.textContent = t(value);
-				break;
-			
-			case 'feedback.invalidRss':
-				feedback.textContent = t(value);
-				break;
+  switch (value) {
+    case 'feedback.invalidUrl':
+      feedback.textContent = t(value);
+      break;
 
-			default:
-				break;
-	}
+    case 'feedback.conflict':
+      feedback.textContent = t(value);
+      break;
+
+    case 'feedback.invalidRss':
+      feedback.textContent = t(value);
+      break;
+
+      default:
+        break;
+  }
 };
 
 export default (elements, i18n, state) => {
-	//const { form, fields, errorFields } = elements;
-	const formProcess = onChange(state, (path, value) => {
-		switch (path) {
-			case 'form.errors':
-				renderErrorss(elements, i18n, value);
-				break;
+  //const { form, fields, errorFields } = elements;
+  const formProcess = onChange(state, (path, value) => {
+  switch (path) {
+      case 'form.errors':
+        renderErrorss(elements, i18n, value);
+        break;
 
-			default:
-				break;
+      default:
+        break;
 
-		}
-	});
+    }
+  });
 
-	return formProcess;
+  return formProcess;
 };
