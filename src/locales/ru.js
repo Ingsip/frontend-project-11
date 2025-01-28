7d1fc4a5c18167b198bb71616112b4d9287aebb1
Ingsip <devsoaring@gmail.com>
2025-01-28 23:40:39 +0300

    fix locales/ru.js

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,14 +1,13 @@
 export default {
-	translation: {
-		feedback: {
-			success: 'RSS успешно загружен',
-			conflict: 'RSS уже существует',
-			notEmpty: 'Не должно быть пустым',
-			invalidUrl: 'Ссылка должна быть валидным URL',
-			invalidRss: 'Ресурс не содержит валидный RSS',
-			viewing: 'Просмотр',
-			networkError: 'Ошибка сети',
-			},
-
-		},
+  translation: {
+    feedback: {
+      success: 'RSS успешно загружен',
+      conflict: 'RSS уже существует',
+      notEmpty: 'Не должно быть пустым',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      invalidRss: 'Ресурс не содержит валидный RSS',
+      viewing: 'Просмотр',
+      networkError: 'Ошибка сети',
+      },
+    },
 };
